fc3827a02817a7194bdc6b70b0675923883f305a
William Low <wlwleong@gmail.com>
2022-03-20 18:39:22 +0800

    main for testing

---
+++ main_get_path.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_simple.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlwleong <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 01:12:58 by wlwleong          #+#    #+#             */
+/*   Updated: 2022/03/20 14:38:06 by wlwleong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_get_cmd_path(char *cmd);
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("Invalid input!\n");
+		return (-1);
+	}
+	printf("output at main : %s\n", ft_get_cmd_path(argv[1]));
+	return (0);
+}
