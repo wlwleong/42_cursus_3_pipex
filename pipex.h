9d45fcbfab4b6a0bf2234737cf70475b98f79f35
William Low <wlow@u90z06s03.42kl.edu.my>
2022-03-20 18:28:52 +0800

    header file normed

--- pipex.h
+++ pipex.h
@@ -13,11 +13,11 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
 
 char	**ft_split(char const *s, char c);
 char	*ft_get_cmd_path(char *cmd);
